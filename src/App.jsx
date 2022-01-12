d45d5cdf55989072d1ac683afae4b01c4f70e19c
Katie Schrattenholzer <ktschr11@gmail.com>
2022-01-12 15:09:47 -0800

    selected date renders to screen when date is changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,47 @@
 import { useState } from 'react';
 
-function useTimeTravel(initialState) {
+export default function App() {
+
+//hoook
+
+  function useTimeTravel() {
   
-  const [state, setState] = useState(initialState);
+  // const [state, setState] = useState(initialState);
+  const [currVal, setCurrVal] = useState('');
+  const [prevVal, setPrevVal] = useState();
+  const [dates, setDates] = useState([]);
 
   //will add:
   //save function to set and save a new current value
-  //undo to set the current value to the previous value
-  //redo to set the current value to the next value
+  const save = (e) => {
+    setCurrVal(e.target.value);
+    
+    // dates.push(e.target.value);
+  }
 
+  //undo to set the current value to the previous value
+  const undo = () => {
+
+  }
+
+  //redo to set the current value to the next value
+  const redo = () => {
+
+  }
+
+  return [currVal, prevVal, dates, save, undo, redo]
 }
 
-export default function App() {
+///normal App
 
-  const [state, save, undo, redo] = useTimeTravel({
-    currVal: '',
-    prevVal: '',
-    dates: [],
-  })
-  
+  const [currVal, prevVal, dates, save, undo, redo ] = useTimeTravel();
+
   return (
   <div>
     <button>undo</button>
     <button>redo</button>
-    <input type="date" aria-label='date' />
-    <p>please select a date</p>
+    <input type="date" aria-label='date' value={currVal} onChange={save} />
+    {currVal ? ( currVal ) : <p>please select a date</p>}
   </div>
     );
 }
